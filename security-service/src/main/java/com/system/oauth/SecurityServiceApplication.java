6b55aeb41e2b18bfc53b49ff5987f510320342e7
Alelizzt <alejandrolistc@hotmail.com>
2025-01-21 17:12:25 -0500

    Add a password encoder and RSA configs.

--- security-service/src/main/java/com/system/oauth/SecurityServiceApplication.java
+++ security-service/src/main/java/com/system/oauth/SecurityServiceApplication.java
@@ -1,13 +1,25 @@
 package com.system.oauth;
 
+import com.system.oauth.config.RsaKeysConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+//Habilita la carga de propiedades de claves RSA
+@EnableConfigurationProperties(RsaKeysConfig.class)
 public class SecurityServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityServiceApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
